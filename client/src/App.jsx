68df50d853db08bcfd306a5b0ce03a767330065a
prakashaditya13 <prakashaditya@gamil.com>
2023-03-21 04:46:44 +0530

    layout finish

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import {Home, CreatePost} from './pages'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+   <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-[#e6ebf4]">
+      <Link to='/'>
+        <h2 className="text-[20px] font-normal">Image<span className="text-cyan-300 font-bold">Gen</span>/<span className="font-normal">AI</span></h2>
+      </Link>
 
-export default App
+      <Link to='/create-post' className="font-inter font-medium bg-[#6469ff] text-white px-3 py-1 rounded-md">Create</Link>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/create-post" element={<CreatePost/>} />
+      </Routes>
+    </main>
+   </BrowserRouter>
+  );
+};
+
+export default App;
