11ae4b5c59b255a5eb98ef83936e0ad55695950e
prakashaditya13 <prakashaditya@gamil.com>
2023-03-21 10:00:39 +0530

    updated the post route

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,40 @@
-import express  from "express";
-import * as dotenv from 'dotenv'
-import {v2 as cloudinary} from 'cloudinary'
-import Post from '../mongodb/models/post.js'
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
-const router = express.Router() 
+const router = express.Router();
 
-export default router
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//GET ALL POSTS
+router.route("/").get(async (req, res) => {});
+
+//CREATE A POST
+router.route("/").post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoURL = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoURL.url,
+    });
+
+    res.status(201).json({ succes: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error,
+    });
+  }
+});
+
+export default router;
